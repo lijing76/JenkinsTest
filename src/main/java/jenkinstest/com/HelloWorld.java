57781e47eaeb1e76e6000f3f48e5aa13fc80c808
Jing Li <jingli2@cisco.com>
2021-11-01 09:26:58 +0800

    add haha

--- src/main/java/jenkinstest/com/HelloWorld.java
+++ src/main/java/jenkinstest/com/HelloWorld.java
@@ -3,5 +3,6 @@ package jenkinstest.com;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! 1");
+        System.out.println("haha");
     }
 }
